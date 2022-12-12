f5f8a4579e8b750bcbc216ef789c5b02af028c8e
karenyoon <yoonyoonsung4@gmail.com>
2022-12-12 17:40:17 +0900

    fastlms 초기화 설정

--- src/main/java/com/zerobase/fastlms/MainPage.java
+++ src/main/java/com/zerobase/fastlms/MainPage.java
@@ -10,10 +10,15 @@ package com.zerobase.fastlms;
 // 1. 클래스 (속성. 메소드로 이루어짐) -- 새로 생길 때마다 클래스가 계속 생겨야 됨 !!탈락!!
 //
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@Controller
 public class MainPage {
     @RequestMapping("/")
     public String index() {
@@ -21,15 +26,43 @@ public class MainPage {
         return "Index Page";
 
     }
+
+
+
+    // 스프링 -> MVC (View -> 템플릿엔진 화면에 내용을 출력(html))
+    // .NET -> MVC (View -> 출력)
+    // python django -> MTV(T = Template -> 화면출력)
+    // 백엔드과정 -> View 까진 가지 않음 / 프론트엔드 과정
+    // BUT . 백엔드도 WEB 이기 때문에 알아야 됨
+    // V -> HTML ==> 웹페이지가
+    // V -> json ==> API
+    // 기본적인 개념은 꼭 알아야 됨
+
+
+
+    //request -> WEB -> SERVER
+    //response -> SERVER -> WEB
+
+
     @RequestMapping("/hello")
-    public String hello () {
+    public void hello (HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+
+        PrintWriter printWriter = response.getWriter();
+
         String msg =  "<html>" +
                 "<head>" +
+                "<meta charset=\"UTF-8\">" +
                 "</head>" +
                 "<body>" +
-                "<p>hello</p> <p>fastlms website!!</p>" +
+                "<p>hello</p>" +
+                "<p>fastlms website!!</p>" +
+                "<p>안녕하세요!!!!<p>" +
                 "</body>" +
                 "</html>";
-        return msg;
+
+        printWriter.write(msg);
+        printWriter.close();
     }
 }
